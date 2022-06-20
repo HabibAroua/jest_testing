7e7e802538146cd5bbff1c331d63b6bad9e2b318
Habib Aroua <habib.a@xelero.io>
2022-06-20 10:14:36 +0100

    Test function with Async/Await

--- sum.test.js
+++ sum.test.js
@@ -2,6 +2,7 @@ const sum =  require('./sum')
 const object = require('./object');
 const string = require('./string');
 const fetchData = require('./fetchData');
+const fetchData1 = require('./fetchData1');
 
 test("test case",()=>{
     expect(sum()).toBe(3)
@@ -20,3 +21,8 @@ test("promise testing",()=>{
         expect(data).toBe('hello')
     })
 });
+
+test("test async function", async ()=>{
+    const data = await fetchData1()
+    expect(data).toBe('done');
+})
